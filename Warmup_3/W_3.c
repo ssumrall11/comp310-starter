6ab572bb0d7c5eddc6e185ace8fabbaa987ecd61
Sarah Sumrall <ssumrall@luc.edu>
2024-01-18 21:40:56 +0000

    More Commits

---
+++ Warmup_3/W_3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+    int nums[] = {1, 2, 3, 4, 5};
+    int sum = 0;
+    for(int u = 0; u < 5; u++){
+        sum += nums[u];
+    }
+    double avg = sum / (double) 4;
+    printf("Sum: %d\n", sum);
+    printf("Average: %lf\n", avg);
+}
